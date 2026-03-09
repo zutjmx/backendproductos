0abce593c2448b0a07f6c9d2b0a4301b61de9f32
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2026-03-09 16:14:57 -0600

    Add CrossOrigin annotation to ProductoRepository for CORS support

--- src/main/java/com/zutjmx/curso/spring/jpabackend/backendproductos/repositories/ProductoRepository.java
+++ src/main/java/com/zutjmx/curso/spring/jpabackend/backendproductos/repositories/ProductoRepository.java
@@ -2,9 +2,11 @@ package com.zutjmx.curso.spring.jpabackend.backendproductos.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.zutjmx.curso.spring.jpabackend.backendproductos.entities.Producto;
 
+@CrossOrigin(originPatterns = "*")
 @RepositoryRestResource(path = "productos")
 public interface ProductoRepository extends CrudRepository<Producto, Long> {
 
