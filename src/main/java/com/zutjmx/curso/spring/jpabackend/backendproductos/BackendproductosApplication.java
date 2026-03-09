afc975fbb0dd8f63701e1263c8ec6c4046a830db
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2026-03-09 12:25:50 -0600

    first commit

---
+++ src/main/java/com/zutjmx/curso/spring/jpabackend/backendproductos/BackendproductosApplication.java
@@ -0,0 +1,13 @@
+package com.zutjmx.curso.spring.jpabackend.backendproductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendproductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendproductosApplication.class, args);
+	}
+
+}
