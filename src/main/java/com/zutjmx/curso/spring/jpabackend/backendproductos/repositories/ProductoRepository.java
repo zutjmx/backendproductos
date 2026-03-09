59c337cd759154a61f7dc300c92fd8c9eac28721
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2026-03-09 13:28:09 -0600

    Add ProductoRepository interface and initial mock data for products

--- src/main/java/com/zutjmx/curso/spring/jpabackend/backendproductos/repositories/ProductoRepository.java
+++ src/main/java/com/zutjmx/curso/spring/jpabackend/backendproductos/repositories/ProductoRepository.java
@@ -1,9 +1,11 @@
 package com.zutjmx.curso.spring.jpabackend.backendproductos.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.zutjmx.curso.spring.jpabackend.backendproductos.entities.Producto;
 
+@RepositoryRestResource(path = "productos")
 public interface ProductoRepository extends CrudRepository<Producto, Long> {
 
 }
